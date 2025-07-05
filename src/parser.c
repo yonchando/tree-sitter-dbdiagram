fc8fc30a2ff83568694a85a4354d111cadaf70a6
Yon Chando <von.chando@gmail.com>
2025-07-05 20:16:13 +0700

    Change name

--- src/parser.c
+++ src/parser.c
@@ -2926,7 +2926,7 @@ extern "C" {
 #define TS_PUBLIC __attribute__((visibility("default")))
 #endif
 
-TS_PUBLIC const TSLanguage *tree_sitter_dbdiagram(void) {
+TS_PUBLIC const TSLanguage *tree_sitter_dbml(void) {
   static const TSLanguage language = {
     .abi_version = LANGUAGE_VERSION,
     .symbol_count = SYMBOL_COUNT,
@@ -2951,7 +2951,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_dbdiagram(void) {
     .lex_modes = (const void*)ts_lex_modes,
     .lex_fn = ts_lex,
     .primary_state_ids = ts_primary_state_ids,
-    .name = "dbdiagram",
+    .name = "dbml",
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
